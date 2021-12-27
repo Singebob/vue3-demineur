0a079aeee45c20b552ee974fbf249cdd9e8ac7c4
Jeremy Chauvin <jeremie.chauvin@epsi.fr>
2021-12-27 09:34:14 +0000

    :rotating_light: init eslint and prettier

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,9 @@
 module.exports = {
-    moduleFileExtensions: [
-      'js',
-      'ts',
-      'json',
-      'vue'
-    ],
-    preset: 'ts-jest',
-    testEnvironment: 'jsdom',
-    transform: {
-      '^.+\\.ts$': 'ts-jest',
-      '^.+\\.vue$': '@vue/vue3-jest'
-    }
-  }
+  moduleFileExtensions: ['js', 'ts', 'json', 'vue'],
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.vue$': '@vue/vue3-jest',
+  },
+}
